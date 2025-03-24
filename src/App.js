218f32f23347de2ab0a3e675f1ab45bd8b8dc700
ying29ho <ying29ho@gmail.com>
2025-03-24 12:29:22 +0800

    added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './homepage';
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HOME PAGE
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
